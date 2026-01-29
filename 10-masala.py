085c808cb7be143b23428dc29475739daef17677
abdullohshifaa-lab <abdullohshifaa@gmail.com>
2026-01-29 11:07:40 +0300

    amaliy-vazifa

--- 10-masala.py
+++ 10-masala.py
@@ -7,16 +7,18 @@ class Product:
         self.price = price
 
 class Laptop(Product):
-    def __init__(self, title, price):
+    def __init__(self, title, price, cpu_model):
         super().__init__(title, price)
+        self.cpu_model = cpu_model
 
 laptops = [
-    Laptop("MacBook Pro", 2200),
-    Laptop("HP Spectre", 1800),
-    Laptop("Lenovo", 1700)
+    Laptop("MacBook Pro", 2200, "M2"),
+    Laptop("HP Spectre", 1800, "Intel i7"),
+    Laptop("Lenovo", 1700, "Intel i5")
 ]
 
 eng_qimmat = max(laptops, key=lambda laptop: laptop.price)
 
 print(f"{eng_qimmat.title}")
 print(f"Narxi: {eng_qimmat.price}")
+print(f"cpu_modeli: {eng_qimmat.cpu_model}")
